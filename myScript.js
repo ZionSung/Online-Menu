60a8348ab79989c94147fab873c741141d3ff5b5
Zion Sung <zionsung@ZiondeMacBook-Pro.local>
2021-03-24 12:12:20 +0800

    finish soup

--- myScript.js
+++ myScript.js
@@ -3,6 +3,11 @@ var check_one_appetizer = 0;
 var last_total_price = 0;
 var check_one_wine = 0;
 var w_a = 0, w_b = 0, w_c = 0;
+var check_one_meal = 0;
+var m_a = 0, m_b = 0, m_c = 0;
+var soup_record = "";
+var s_a = 0, s_b = 0, s_c = 0;
+
 
 document.getElementById("total_order_price").innerHTML = "Total $ " + total_price;
 
@@ -112,4 +117,116 @@ function wine_c(){
     w_c = 1;
     renew_total_price()
 }
+
+document.getElementById("add_meal_a").addEventListener("click", meal_a);
+
+function meal_a(){
+    document.getElementById("meal").innerHTML = "原塊牛排 $450";
+    if( check_one_meal == 0 ){
+        total_price = total_price + 450;
+        check_one_meal = 1;
+        m_a = 1;
+    }
+    else{
+        if( m_b == 1 ){
+            total_price = total_price - 425 + 450;
+            m_b = 0;
+        }
+        else if( m_c == 1 ){
+            total_price = total_price - 510 + 450;
+            m_c = 0;
+        }
+    }
+    m_a = 1;
+    renew_total_price()
+}
+
+document.getElementById("add_meal_b").addEventListener("click", meal_b);
+
+function meal_b(){
+    document.getElementById("meal").innerHTML = "義式丁骨豬排 $425";
+    if( check_one_meal == 0 ){
+        total_price = total_price + 425;
+        check_one_meal = 1;
+        m_b = 1;
+    }
+    else{
+        if( m_a == 1 ){
+            total_price = total_price - 450 + 425;
+            m_a = 0;
+        }
+        else if( m_c == 1 ){
+            total_price = total_price - 510 + 425;
+            m_c = 0;
+        }
+    }
+    m_b = 1;
+    renew_total_price()
+}
+
+document.getElementById("add_meal_c").addEventListener("click", meal_c);
+
+function meal_c(){
+    document.getElementById("meal").innerHTML = "時蔬厚切燉牛排 $510";
+    if( check_one_meal == 0 ){
+        total_price = total_price + 510;
+        check_one_meal = 1;
+        m_c = 1;
+    }
+    else{
+        if( m_a == 1 ){
+            total_price = total_price - 450 + 510;
+            m_a = 0;
+        }
+        else if( m_b == 1 ){
+            total_price = total_price - 425 + 510;
+            m_b = 0;
+        }
+    }
+    m_c = 1;
+    renew_total_price()
+}
+
+document.getElementById("add_soup_a").addEventListener("click",function(){soup(0);});
+document.getElementById("add_soup_b").addEventListener("click",function(){soup(1);});
+document.getElementById("add_soup_c").addEventListener("click",function(){soup(2);});
+
+function soup( soup_num ){
+
+    if( soup_num == 0 ){
+        soup_record = soup_record + '杏苞菇南瓜濃湯 $120' + "<br>";
+        document.getElementById("soup").innerHTML = soup_record;
+        total_price = total_price + 120;
+        /*
+        if( s_a == 0 ){
+            total_price = total_price + 120;
+            s_a = 1;
+        }
+        */
+    }
+    else if( soup_num == 1 ){
+        soup_record = soup_record + '義式海鮮清湯 $120' + "<br>";
+        document.getElementById("soup").innerHTML = soup_record;
+        total_price = total_price + 120;
+        /*
+        if( s_b == 0 ){
+            total_price = total_price + 120;
+            s_b = 1;
+        }
+        */
+    }
+    else if( soup_num == 2 ){
+        soup_record = soup_record + '雞肉巧達濃湯 $120' + "<br>";
+        document.getElementById("soup").innerHTML = soup_record;
+        total_price = total_price + 120;
+        /*
+        if( s_c == 0 ){
+            total_price = total_price + 120;
+            s_c = 1;
+        }
+        */
+    }
+    renew_total_price()
+}
+
 
